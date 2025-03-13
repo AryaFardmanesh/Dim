748ec78f3617abd69ff331414d9f6a7b19a41e8f
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-03-13 18:10:33 +0330

    Add: some functions for string lib.

--- src/libs/string.h
+++ src/libs/string.h
@@ -33,6 +33,36 @@ static JSValue string_strcpy(JSContext* ctx, JSValueConst this_val, int argc, JS
 	return JS_NewString(context, new_str);
 }
 
+static JSValue string_strcat(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst* argv) {
+	if (argc < 2) {
+		return JS_ThrowTypeError(ctx, "[DIM]: ArgumentError: The 'strcat' function expected two argument.\n");
+	}
+
+	char* str1 = (char*)JS_ToCString(ctx, argv[0]);
+	char* str2 = (char*)JS_ToCString(ctx, argv[1]);
+	char* new_str = strcat(str1, str2);
+
+	JS_FreeCString(context, str1);
+	JS_FreeCString(context, str2);
+
+	return JS_NewString(context, new_str);
+}
+
+static JSValue string_strcmp(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst* argv) {
+	if (argc < 2) {
+		return JS_ThrowTypeError(ctx, "[DIM]: ArgumentError: The 'strcmp' function expected two argument.\n");
+	}
+
+	char* str1 = (char*)JS_ToCString(ctx, argv[0]);
+	char* str2 = (char*)JS_ToCString(ctx, argv[1]);
+	int cmp = (strcmp(str1, str2) == 0) ? 1 : 0;
+
+	JS_FreeCString(context, str1);
+	JS_FreeCString(context, str2);
+
+	return JS_NewBool(context, cmp);
+}
+
 void string_module_init() {
 	struct DIM_MODULE_LIST* module = dim_module_create_module(MODULE_NAME, NULL);
 	dim_module_append_module(module);
@@ -42,6 +72,12 @@ void string_module_init() {
 
 	struct DIM_MODULE_FUNCS* string_strcpy_s = dim_module_create_func("strcpy", string_strcpy);
 	dim_module_append_func(MODULE_NAME, string_strcpy_s);
+
+	struct DIM_MODULE_FUNCS* string_strcat_s = dim_module_create_func("strcat", string_strcat);
+	dim_module_append_func(MODULE_NAME, string_strcat_s);
+
+	struct DIM_MODULE_FUNCS* string_strcmp_s = dim_module_create_func("strcmp", string_strcmp);
+	dim_module_append_func(MODULE_NAME, string_strcmp_s);
 }
 
 #undef MODULE_NAME
