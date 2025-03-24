7816ae8dac7c1b7be2558a70b36b0737968e133d
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-03-24 23:52:09 +0330

    Add: input function in stdio module.

--- src/libs/stdio.h
+++ src/libs/stdio.h
@@ -6,6 +6,7 @@
 #include "../module/module.h"
 #include "../dim.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 static JSValue stdio_echo(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst* argv) {
 	if (argc < 1) {
@@ -19,12 +20,36 @@ static JSValue stdio_echo(JSContext* ctx, JSValueConst this_val, int argc, JSVal
 	return JS_UNDEFINED;
 }
 
+static JSValue stdio_input(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst* argv) {
+	if (argc >= 1) {
+		char* value = (char*)JS_ToCString(ctx, argv[0]);
+		printf("%s", value);
+		JS_FreeCString(context, value);
+	}
+
+	size_t str_size = 1;
+	char* str = (char*)malloc(str_size);
+	char ch = (char)0x0;
+
+	while ((int)(ch = (char)getchar()) != (int)'\n') {
+		str[str_size - 1] = ch;
+		str = (char*)realloc(str, ++str_size);
+	}
+
+	str[str_size - 1] = (char)0x0;
+
+	return JS_NewString(context, (const char*)str);
+}
+
 void stdio_module_init() {
 	struct DIM_MODULE_LIST* module = dim_module_create_module(MODULE_NAME, NULL);
 	dim_module_append_module(module);
 
-	struct DIM_MODULE_FUNCS* stdio_printf_struct = dim_module_create_func("echo", stdio_echo);
-	dim_module_append_func(MODULE_NAME, stdio_printf_struct);
+	struct DIM_MODULE_FUNCS* stdio_echo_struct = dim_module_create_func("echo", stdio_echo);
+	dim_module_append_func(MODULE_NAME, stdio_echo_struct);
+
+	struct DIM_MODULE_FUNCS* stdio_input_struct = dim_module_create_func("input", stdio_input);
+	dim_module_append_func(MODULE_NAME, stdio_input_struct);
 }
 
 #undef MODULE_NAME
